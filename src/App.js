abe0b0bd3372cb578b6e0be0aeac57a88d5f5304
Shrishyle Pandit <64643330+shrishyle@users.noreply.github.com>
2024-06-03 02:44:28 +0530

    Created Code for User, Portfolio, Wishlist, Stock etc.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import MarketUpdate from './pages/MarketUpdate';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import MyPortfolio from './pages/MyPortfolio';
-import MyWishlist from './pages/MyWishlist';
-import AboutUs from './pages/FooterPages/AboutUs';
-import Careers from './pages/FooterPages/Careers';
-import InvestorRelations from './pages/FooterPages/InvestorRelations';
-import SecurityInformation from './pages/FooterPages/SecurityInformation';
-import ContactUs from './pages/FooterPages/ContactUs';
-import FAQ from './pages/FooterPages/FAQ';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import MarketUpdate from "./pages/MarketUpdate";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import MyPortfolio from "./pages/MyPortfolio";
+import MyWishlist from "./pages/MyWishlist";
+import AboutUs from "./pages/FooterPages/AboutUs";
+import Careers from "./pages/FooterPages/Careers";
+import InvestorRelations from "./pages/FooterPages/InvestorRelations";
+import SecurityInformation from "./pages/FooterPages/SecurityInformation";
+import ContactUs from "./pages/FooterPages/ContactUs";
+import FAQ from "./pages/FooterPages/FAQ";
 // import PrivateRoute from './Components/PrivateRoute'; // Comment out this import if not using it
+import { stock } from "./Data/data2";
 
 function App() {
   const handleGoogleLoginSuccess = (response) => {
-    console.log('Google Login Success:', response);
+    console.log("Google Login Success:", response);
     // Handle login success (e.g., send token to your server or store it locally)
   };
 
   const handleGoogleLoginFailure = (response) => {
-    console.log('Google Login Failed:', response);
+    console.log("Google Login Failed:", response);
     // Handle login failure
   };
 
@@ -49,7 +50,6 @@ function App() {
 }
 
 export default App;
-
 
 //import React from 'react';
 //import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
--- src/Data/data2.js
+++ src/Data/data2.js
@@ -1,68 +1,104 @@
+import { indexOf } from "lodash";
+
 class User {
   constructor(userName, email) {
     this.userName = userName;
     this.email = email;
-    
-
-    this.portfolio = () => {
-      let myPortfolio = new Portfolio();
-      return myPortfolio;
+    this.wishlists = [];
+    this.portfolio = new Portfolio();
+    this.createNewWishlist = (name) => {
+      let myWishlist = new Wishlist(name);
+      this.wishlists.push(myWishlist);
     };
-
-    this.showPortfolio = () => {
-      console.log(this.portfolio);
-    };
-
-    this.wishlist = () => {
-      let myWishlist = new Wishlist();
-      return myWishlist;
-    };
-
-    this.showWishlist = () => {
-      console.log(this.portfolio);
-    };
+    this.showPortfolio = () => console.log(this.portfolio());
+    this.displayWishLists = () => console.log(this.wishlists);
   }
 }
 
 class Portfolio {
   constructor() {
     this.list = [];
-    this.addStock = (stock) => {
-      let stockItem = new Stock(stock);
+    this.addStock = (stockName) => {
+      let stockItem = new Stock(stockName);
       this.list.push(stockItem);
     };
     this.deleteStock = () => {
-      this.list.pop();
+      this.list.forEach((item) => {
+        console.log(item.totalQuantity);
+        if (item.totalQuantity === 0) {
+          let index = indexOf(item);
+          delete this.list[index];
+        }
+      });
+
     };
-    this.displayList = () => {
-      console.log(this.list);
-    };
+    this.displayList = () => console.log(this.list);
+    // this.deleteStock();
   }
 }
 
 class Wishlist {
-  constructor() {
+  constructor(name) {
+    this.name = name;
     this.list = [];
-    this.addStock = (stock) => {
-      let stockItem = new Stock(stock);
+    this.addStock = (stockName) => {
+      let stockItem = new Stock(stockName);
       this.list.push(stockItem);
     };
     this.deleteStock = () => {
       this.list.pop();
     };
-    this.displayList = () => {
-      console.log(this.list);
-    };
+    this.displayList = () => console.log(this.list);
   }
 }
 
 class Stock {
-  constructor(name, stockSymbol) {
+  constructor(name) {
     this.name = name;
-    this.stockSymbol = stockSymbol;
+    this.transactionStatement = [];
+    this.totalQuantity = 0;
+    this.totalCost = 0;
+    this.averageCost = 0;
+    this.buyStock = (quantity, rate) => {
+      let transaction = ["buy", new Date(), quantity, rate];
+      this.transactionStatement.push(transaction);
+      this.totalQuantity = this.totalQuantity + quantity;
+      let amount = quantity * rate;
+      this.totalCost = this.totalCost + amount;
+      this.averageCost = this.totalCost / this.totalQuantity;
+    };
+    this.sellStock = (quantity) => {
+      if (this.totalQuantity >= 0) {
+        let transaction = ["sell", new Date(), quantity, this.averageCost];
+        this.transactionStatement.push(transaction);
+        this.totalQuantity = this.totalQuantity - quantity;
+        let amount = quantity * this.averageCost;
+        this.totalCost = this.totalCost - amount;
+        if (this.totalCost !== 0) {
+          this.averageCost = this.totalCost / this.totalQuantity;
+        } else {
+          this.averageCost = 0;
+        }
+      }
+    };
+    this.displayTotalQuantity = () => console.log(this.totalQuantity);
+    this.displayAverageCost = () => console.log(this.averageCost);
+    this.displayTransactionStatement = () => console.log(this.transactionStatement);
   }
 }
 
-let stock = new Stock("Tata", "TAT");
-let wishlist1 = new Wishlist();
-wishlist1.addStock(stock);
+// Code for Testing the above code
+// let shrishyle = new User("shrishyle", "shrishyle.pandit@gmail.com");
+// shrishyle.portfolio.addStock("Tata");
+// shrishyle.portfolio.list[0].buyStock(100, 20);
+// shrishyle.portfolio.list[0].buyStock(50, 25);
+// shrishyle.portfolio.list[0].buyStock(50, 30);
+// shrishyle.portfolio.list[0].sellStock(50);
+// shrishyle.portfolio.displayList();
+// shrishyle.portfolio.list[0].displayAverageCost();
+// shrishyle.portfolio.list[0].displayTotalQuantity();
+// shrishyle.portfolio.list[0].sellStock(150);
+// shrishyle.portfolio.list[0].displayTransactionStatement();
+// shrishyle.portfolio.displayList();
+
+export { User };
