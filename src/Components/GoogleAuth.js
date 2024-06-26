e2c9b76ca36db5eb714eded2072aee9a295ec721
ISNLP <indiwar1@rediffmail.com>
2024-06-26 14:16:13 -0400

    Login, Google Auth changes

--- src/Components/GoogleAuth.js
+++ src/Components/GoogleAuth.js
@@ -1,16 +1,117 @@
 import React from 'react';
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import { useAuth } from '../context/AuthContext';
+import { useUser } from '../context/UserContext';
 
-const GoogleAuth = ({ onSuccess, onFailure }) => {
+import { useNavigate } from 'react-router-dom';
+function GoogleLoginComponent(props) {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const { setUsername } = useUser();
+  const googleLogin = useGoogleLogin({
+    onSuccess: async tokenResponse => {
+      
+      // Get user information from Google
+      const userInfo = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+          Authorization: `Bearer ${tokenResponse.access_token}`
+        }
+      });
+      console.log('User Info:', userInfo.data);
+      debugger;
+          const response = await fetch('https://backend-repo-equitywise.onrender.com/FindUser', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ emailid: userInfo.data.email })
+          });
+        debugger;
+      if (response.status === 201) {
+              axios.post('https://backend-repo-equitywise.onrender.com/signup', {
+              emailid: userInfo.data.email,
+              password: userInfo.data.email
+            })
+            .then(function (response) {
+              console.log('API response:', response);
+              console.log('Signup Success:', response);
+              login(response.data);
+            
+            })
+            .catch(function (error) {
+              console.log('API error:', error);
+            // onFailure(error);
+            });
+          }
+          else{
+            debugger;
+            if(props.Login === "true"){
+              axios.post('https://backend-repo-equitywise.onrender.com/login', {
+                emailid: userInfo.data.email,
+                password: userInfo.data.email
+              })
+              .then(function (response) {
+                debugger;
+                console.log('API response:', response);
+                if (response.status === 201 || response.status === 200) {
+                 // onSuccess(response);
+                 login(response.data);
+                } else {
+                  //setErrorMessage('Invalid email or password');
+                }
+              })
+              .catch(function (error) {
+                debugger;
+                console.log('API error:', error);
+               // onFailure(error);
+              });
+            }
+            else{
+              login(response.data);
+            }
+          }
+      // Handle user information (e.g., store in state or context)
+      setUsername(userInfo.data.email);
+     
+       navigate('/MyPortfolio');
+    },
+    onError: errorResponse => {
+      console.error('Login Failed:', errorResponse);
+    }  
+   
+  });
+
   return (
-    <GoogleOAuthProvider clientId="536138454360-vu6gndr3s5qrao17q08p57fvrs9lj2ee.apps.googleusercontent.com">
-      <GoogleLogin
-        onSuccess={onSuccess}
-        onFailure={onFailure}
-        cookiePolicy={'single_host_origin'}
-      />
-    </GoogleOAuthProvider>
+    <div className="flex flex-col items-center">
+      <button className="text-blue-500 border border-blue-500 rounded py-2 px-4 hover:bg-blue-500 hover:text-white" onClick={googleLogin}>Login with Google</button>
+    </div>
   );
-};
+}
 
-export default GoogleAuth;
+
+
+export default GoogleLoginComponent;
+
+
+
+
+
+
+
+//import React from 'react';
+//import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+
+//const GoogleAuth = ({ onSuccess, onFailure }) => {
+//  return (
+//    <GoogleOAuthProvider clientId="536138454360-vu6gndr3s5qrao17q08p57fvrs9lj2ee.apps.googleusercontent.com">
+//      <GoogleLogin
+//        onSuccess={onSuccess}
+//        onFailure={onFailure}
+//        cookiePolicy={'single_host_origin'}
+//      />
+//    </GoogleOAuthProvider>
+//  );
+//};
+
+//export default GoogleAuth;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { useUser } from '../context/UserContext';
 import GoogleLoginComponent from '../Components/GoogleAuth';
+
 const Login = () => {
   const { login } = useAuth();
   const [email, setEmail] = useState('');
@@ -15,8 +16,8 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate(); 
-
   const { setUsername } = useUser();
+
   const onSuccess = (response) => {
     console.log('Login Success:', response);
     setUsername(email);
@@ -43,8 +44,7 @@ const Login = () => {
       emailid: email,
       password: password
     })
-    .then(function (response) {
-      debugger;
+    .then((response) => {
       console.log('API response:', response);
       if (response.status === 201 || response.status === 200) {
         onSuccess(response);
@@ -52,8 +52,7 @@ const Login = () => {
         setErrorMessage('Invalid email or password');
       }
     })
-    .catch(function (error) {
-      debugger;
+    .catch((error) => {
       console.log('API error:', error);
       onFailure(error);
     });
@@ -101,9 +100,9 @@ const Login = () => {
               <p>Not registered with Equity Wise? <Link to="/signup" className="text-blue-500">Sign Up here</Link></p>
             </div>
             <div className='flex justify-center'>
-            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-               <GoogleLoginComponent/>
-            </GoogleOAuthProvider>
+              <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+                <GoogleLoginComponent login={true} />
+              </GoogleOAuthProvider>
             </div>
           </div>
         </div>
@@ -113,3 +112,124 @@ const Login = () => {
 };
 
 export default Login;
+
+
+
+
+
+
+//import React, { useState } from 'react';
+//import axios from 'axios';
+//import { GoogleOAuthProvider } from '@react-oauth/google';
+//import { useAuth } from '../context/AuthContext';
+//import { Link, useNavigate } from 'react-router-dom';
+//import image from '../assets/images/login.png';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+//import { useUser } from '../context/UserContext';
+//import GoogleLoginComponent from '../Components/GoogleAuth';
+//const Login = () => {
+//  const { login } = useAuth();
+//  const [email, setEmail] = useState('');
+//  const [password, setPassword] = useState('');
+//  const [showPassword, setShowPassword] = useState(false);
+//  const [errorMessage, setErrorMessage] = useState('');
+//  const navigate = useNavigate(); 
+
+//  const { setUsername } = useUser();
+//  const onSuccess = (response) => {
+//    console.log('Login Success:', response);
+//    setUsername(email);
+//    login(response.data);
+//    navigate('/MyPortfolio');
+//  };
+
+//  const onFailure = (response) => {
+//    console.log('Login Failed:', response);
+//    setErrorMessage('Login failed. Please try again.');
+//  };
+
+//  const handleEmailLogin = () => {
+//    console.log('handleEmailLogin called');
+
+//    if (!email || !password) {
+//      setErrorMessage('Please enter both email and password');
+//      console.log('Email or password not entered');
+//      return;
+//    }
+
+//    console.log('Validation passed. Making API request...');
+//    axios.post('https://backend-repo-equitywise.onrender.com/login', {
+//      emailid: email,
+//      password: password
+//    })
+//    .then(function (response) {
+//      debugger;
+//      console.log('API response:', response);
+//      if (response.status === 201 || response.status === 200) {
+//        onSuccess(response);
+//      } else {
+//        setErrorMessage('Invalid email or password');
+//      }
+//    })
+//    .catch(function (error) {
+//      debugger;
+//      console.log('API error:', error);
+//      onFailure(error);
+//    });
+//  };
+
+//  return (
+//    <div className="flex flex-col h-screen">
+//      <div className="flex-grow flex justify-center items-center">
+//        <div className="flex flex-col md:flex-row w-full md:w-2/3 p-4 bg-white rounded shadow-md">
+//          <div className="md:w-1/2 p-4 hidden md:block">
+//            <img src={image} alt="Login to Equity Wise" className="w-full h-auto" />
+//          </div>
+//          <div className="md:w-1/2 p-4">
+//            <h1 className="text-2xl font-bold mb-4">Login to Equity Wise</h1>
+//            {errorMessage && <div className="text-red-500 mb-4">{errorMessage}</div>}
+//            <div className="mb-4">
+//              <input
+//                type="email"
+//                placeholder="Email"
+//                className="w-full p-2 border border-gray-300 rounded"
+//                value={email}
+//                onChange={(e) => setEmail(e.target.value)}
+//              />
+//            </div>
+//            <div className="mb-4 relative">
+//              <input
+//                type={showPassword ? "text" : "password"}
+//                placeholder="Password"
+//                className="w-full p-2 border border-gray-300 rounded"
+//                value={password}
+//                onChange={(e) => setPassword(e.target.value)}
+//              />
+//              <button
+//                type="button"
+//                className="absolute right-2 top-2 text-gray-300"
+//                onClick={() => setShowPassword(!showPassword)}
+//              >
+//                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+//              </button>
+//            </div>
+//            <button onClick={handleEmailLogin} className="w-full bg-blue-500 text-white p-2 rounded mb-4">
+//              Login
+//            </button>
+//            <div className="mb-4">
+//              <p>Not registered with Equity Wise? <Link to="/signup" className="text-blue-500">Sign Up here</Link></p>
+//            </div>
+//            <div className='flex justify-center'>
+//            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+//               <GoogleLoginComponent/>
+//            </GoogleOAuthProvider>
+//            </div>
+//          </div>
+//        </div>
+//      </div>
+//    </div>
+//  );
+//};
+
+//export default Login;
