b7539eef72e97f0a1d3f9ccc727fa66ebd48b4c2
pranathi3105 <subhasuresh.y@gmail.com>
2024-06-29 13:46:13 +0530

    gave the full names

--- src/pages/FooterPages/AboutUs.js
+++ src/pages/FooterPages/AboutUs.js
@@ -7,12 +7,12 @@ const AboutUs = () => {
                 <h1 className="text-4xl font-bold mb-4 text-center">About Us</h1>
                 <div className="bg-white shadow-md rounded-lg p-6">
                     <p className="text-lg mb-4">
-                        This app was created by three Pesto students under the mentorship of Chiran:
+                        This app was created by three Pesto students under the mentorship of Chiranjib Nandy:
                     </p>
                     <ul className="list-disc list-inside mb-4 pl-4">
-                        <li className="mb-2">Indi</li>
-                        <li className="mb-2">Subha</li>
-                        <li className="mb-2">Shri</li>
+                        <li className="mb-2">Indiwar Hundal</li>
+                        <li className="mb-2">Subhashini Yenikapati</li>
+                        <li className="mb-2">Shrishyle Narayan Pandit</li>
                     </ul>
                     <p className="text-lg mb-4">
                         The objective behind the app is to enable our users to easily track all their investments in one place.
